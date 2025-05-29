da54e35931d60a065bfa0d3783f2ff00b6672339
hlyanphyo27 <hlyanphyozyx27122003@gmail.com>
2025-05-29 12:43:04 +0630

    last

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -6,33 +6,35 @@ export const workflowClient = new WorkflowClient({
   token: config.env.upstash.qstashToken,
 });
 
-
-export const sendEmail = async ({
-  email,
-  subject,
-  message,
-}: {
+export const sendEmail
+    = async ({
+                                  email,
+                                  subject,
+                                  message,
+                                }: {
   email: string;
   subject: string;
   message: string;
 }) => {
-
   const template = {
     to_email: email,
     title: subject,
     message: message,
-  }
+  };
 
-  const serviceID = process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID;
-  const templateID = process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID;
-  const publicKey = process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY;
-  const privateKey = process.env.NEXT_PUBLIC_EMAIL_PRIVATE_KEY;
-  try{
-    console.log('Sending email...');
-    // @ts-ignore
-    const res = await emailjs.send(serviceID!, templateID!, template, publicKey, privateKey);
-    console.log('Success:', res.text)
-  }catch(error) {
-    console.log('Error:', error.message)
+  const serviceID = process.env.EMAIL_SERVICE_ID!;
+  const templateID = process.env.EMAIL_TEMPLATE_ID!;
+  const publicKey = process.env.EMAIL_PUBLIC_KEY!;
+  const privateKey = process.env.EMAIL_PRIVATE_KEY!;
+
+  try {
+    console.log("Sending email...");
+    const res = await emailjs.send(serviceID, templateID, template, {
+      publicKey,
+      privateKey,
+    });
+    console.log("Success:", res.text);
+  } catch (error) {
+    console.error("Error:", error instanceof Error ? error.message : error);
   }
 };
